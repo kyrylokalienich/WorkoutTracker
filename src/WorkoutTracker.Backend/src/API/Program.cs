09b86d7c74e5317971ea6d5110269fafcea77e06
kosars <155952445+kyrylokalienich@users.noreply.github.com>
2026-05-11 17:48:41 +0300

    docs(swagger): add bearer auth configuration

--- src/WorkoutTracker.Backend/src/API/Program.cs
+++ src/WorkoutTracker.Backend/src/API/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using WorkoutTracker.Application;
 using WorkoutTracker.Application.Configurations;
 using WorkoutTracker.Persistence;
@@ -40,7 +41,33 @@ if (jwtConfig != null)
 builder.Services.AddAuthorization();
 builder.Services.AddControllers();  
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WorkoutTracker API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Paste: Bearer {your JWT token}"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
